26d5c878b8d3cf0ac084236f01bcbdfe63387361
HamzaLatif03 <90351210+HamzaLatif03@users.noreply.github.com>
2022-04-10 14:16:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ laser = pygame.transform.scale(pygame.image.load("laser.png"),(70,60))
 bg = pygame.image.load("background.png")
 #click = mixer.music.load("click.mp3")
 
+player_mask = pygame.mask.from_surface(playerShip)
+Enemy_mask = pygame.mask.from_surface(enemyShip)
+laser_mask = pygame.mask.from_surface(laser)
 
 screen = pygame.display.set_mode((sw, sh))
 pygame.display.set_caption("Hamza's: Space game")
@@ -27,13 +30,13 @@ gameover = False
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, health = 5):
-        #pygame.sprite.Sprite.init(self)
+        pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
-        self.ship_img = playerShip
+        self.image = playerShip
         self.laser = laser
         self.health = health
-        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.mask = pygame.mask.from_surface(self.image)
         self.w = playerShip.get_width()
         self.h = playerShip.get_height()
         self.boost = 100
@@ -76,6 +79,7 @@ class Player(pygame.sprite.Sprite):
         
 class Enemy(Player):
     def __init__(self, x, y,  health = 3):
+        pygame.sprite.Sprite.__init__(self)
         self.image = enemyShip
         self.x = x
         self.y = y
@@ -86,6 +90,7 @@ class Enemy(Player):
         self.alive = True
         self.rect = self.image.get_rect(center = (120, 63))
         self.mask = pygame.mask.from_surface(self.image)
+        
     
     def move(self):
         self.y += self.velocity
@@ -96,16 +101,19 @@ class Enemy(Player):
         if self.x == 746 or self.x == 747 or self.x == 748 or self.x == 749 or self.x == 750:
             self.velocityX = self.velocityX * -1
 
-
+    def xCoordinate(self):
+        return self.x
                 
-
+    def yCoordinate(self):
+        return self.y
+    
     def draw(self):
         if self.y < sh or  self.health == 0:
             self.alive == False
             if self.alive:
                 screen.blit(enemyShip, (self.x, self.y))
                  
-class Bullet(pygame.sprite.Sprite):
+class Bullet():
     def __init__(self, x, y):
         self.x = x + 50
         self.y = y - 10
@@ -120,20 +128,62 @@ class Bullet(pygame.sprite.Sprite):
     def bulletState(self):
         return (self.y < -100)
 
-    def update(self):
-        if pygame.sprite.spritecollide(self, Enemy, True):
-            self.kill()
+    def collision(self):
+        self.hits = pygame.sprite.spritecollide(self, eA, False)
+        if self.hits:
+            print("collide ")
     
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
                 #pygame.time.delay(1) slow downs time potential power up
+                
+class Asteroids(Enemy):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = asteroids
+        self.x = x
+        self. y = y
+        self.alive = True
+        self.direction = random.randint(0,1)
+        
+    def boundaries(self):
+        if self.y > sh or self.y < 0:
+            self.alive == False
+        if self.x > sw or self.x < 0:
+            self.alive == False    
+    
+    def move(self):
+        if self.direction == 1:
+            self.y += random.randint(5,10)
+            self.x += random.randint(5,10)
+        elif self.direction == 0:
+            self.y += random.randint(5,10)
+            self.x -= random.randint(5,10)
+    
 
+    def draw(self):
+        if self.alive == True:
+            screen.blit(self.image, (self.x, self.y))
     
  
 player = Player(sw//2, sh//2)
 enemy = Enemy(random.randint(100,750), random.randint(-50,-40)) 
+asteroidsScreen = Asteroids(500,0)
 enemies= []
 enemies.append(enemy)
+     
+
+allSprites = pygame.sprite.Group()
+eA = pygame.sprite.Group()
+eAS = pygame.sprite.Group()
+B = pygame.sprite.Group()
+P = pygame.sprite.Group()
+
+
+for i in range(6):
+    eA.add(enemy)
+    allSprites.add(enemy)
+allSprites.add(player)     
      
 
 def redrawScreen():
@@ -148,9 +198,11 @@ def redrawScreen():
     keys = pygame.key.get_pressed()
     player.move(keys)
     player.shoot(keys)
+    asteroidsScreen.draw()
+    asteroidsScreen.move()
     for bullet in player.bullets:
+        bullet.collision()
         bullet.draw()
-        bullet.update()
         
     pygame.display.update()
     
@@ -166,7 +218,18 @@ def game():
             
         screen.blit(bg, (0,0))
         
+        if not enemy.alive():
+            enemy = Enemy(random.randint(100,750), random.randint(-50,-40)) 
         
+        for i, inst in enumerate(eA):
+            if inst.rect.y >= (sh - 50):
+                score += 1
+                inst.kill()
+                enemy = Enemy(random.randint(100,750), random.randint(-50,-40)) 
+                eA.add(enemy)
+                allSprites.add(enemy)
+        allSprites.draw(screen)
+        allSprites.update()  
         
         redrawScreen()
         
